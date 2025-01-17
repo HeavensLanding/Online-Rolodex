72008f497fa85b10db1606f5f9773b94c640a092
HeavensLanding <rasmussencourtney0103@gmail.com>
2025-01-17 15:18:53 -0700

    render/listen and script

---
+++ data-syncing.js
@@ -0,0 +1,8 @@
+/**** STATE ****/
+let contactList = []
+let contactToEditId = null
+let contactId = 3
+
+/**** RENDERING & LISTENING ****/
+const contactsContainer = document.getElementById("contacts-container")
+const textarea = document.getElementById("textarea")
