6564194c1b5852f8a96915a3484a21c98dc6bd65
HeavensLanding <rasmussencourtney0103@gmail.com>
2025-01-24 13:40:42 -0700

    render contact list on start

--- data-syncing.js
+++ data-syncing.js
@@ -1,8 +1,22 @@
 /**** STATE ****/
 let contactList = []
 let contactToEditId = null
-let contactId = 3
+let contactId = 2
 
 /**** RENDERING & LISTENING ****/
 const contactsContainer = document.getElementById("contacts-container")
 const textarea = document.getElementById("textarea")
+
+/** Render a list of contacts */
+function renderContactsList() {
+    // Clear out anything from previous renders
+    contactsContainer.innerHTML = ""
+
+    // If there's no reviews, show an empty message
+    if (contactList.length === 0) {
+        contactsContainer.innerHTML = "No contacts yet"
+    }
+
+    // For each review, map it to a div, then append that div to the container
+    contactList.map(renderContactsList).forEach(div => contactsContainer.appendChild(div))
+}
